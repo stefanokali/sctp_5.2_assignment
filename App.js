cd1c137d230fb118fcd1c7dce7344ebaac1bee75
Stefano Kusuma Ali <stefano8795@gmail.com>
2024-08-14 21:32:17 +0800

    finish assignment

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { RadioButton } from "react-native-paper";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [likeCoffee, setLikeCoffee] = useState(false);
+
+  const showAlert = () => {
+    Alert.alert("Summary", `My name is ${name}, I am ${age} years old. ${likeCoffee ? "I like coffee." : "I hate coffee."}`, [
+      {
+        text: "Ok",
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container_textinput}>
+        <Text>Your name:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setName}
+          value={name}
+        />
+      </View>
+      <View style={styles.container_textinput}>
+        <Text>Your age:</Text>
+        <TextInput style={styles.textInput} onChangeText={setAge} value={age} />
+      </View>
+      <View style={styles.container_textinput}>
+        <Text>Like coffee?     </Text>
+        <Text>Yes</Text>
+        <RadioButton
+          value="Yes"
+          status={likeCoffee ? "checked" : "unchecked"}
+          onPress={() => setLikeCoffee(true)}
+        />
+        <Text>No</Text>
+        <RadioButton
+          value="No"
+          status={!likeCoffee ? "checked" : "unchecked"}
+          onPress={() => setLikeCoffee(false)}
+        />
+      </View>
+      <Button title="Submit" onPress={showAlert} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +54,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container_textinput: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: "70%",
   },
 });
